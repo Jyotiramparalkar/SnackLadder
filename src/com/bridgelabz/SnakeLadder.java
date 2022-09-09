b7dc46d49e25b88a515fffe36c79737831c1f1a8
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-09-09 18:05:57 +0530

    Added SnakeLadder UC2 Computation

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeLadder 
 {
 
@@ -7,6 +9,11 @@ public class SnakeLadder
 	{
 		int p =0;
 		System.out.println("Starting position is" +p);
+		
+		// getting die number
+		Random random=new Random();
+		int rolldice=random.nextInt(6)+1;
+		System.out.println("roll die : "+rolldice);
 
 	}
 
