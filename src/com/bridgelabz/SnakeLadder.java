a9e3b0268c16260a93f0a18051aae135f9bade1c
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-09-09 18:18:43 +0530

    Added SnakeladderUC3

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -9,12 +9,48 @@ public class SnakeLadder
 	{
 		int p =0;
 		System.out.println("Starting position is" +p);
-		
+
 		// getting die number
 		Random random=new Random();
 		int rolldice=random.nextInt(6)+1;
 		System.out.println("roll die : "+rolldice);
 
+		//check for the option
+		int option=random.nextInt(3);
+		switch (option) 
+		{
+		case 0:
+			System.out.println("No Play");
+			break;
+
+		case 1:
+			System.out.println("Got ladder");
+			if(p+rolldice <=100) {
+				p = p+rolldice;
+			}
+			break;
+
+		case 2:
+			System.out.println("Got snake");
+			if(p!=0 && p-rolldice>=0 ) 
+			{
+				p = p-rolldice;
+			}
+			else {
+				p=0;
+			}
+			break;
+
+		default :
+			System.out.println("Incorrect  option");
+			break;
+		}
+
+		System.out.println("current position is "+p);
 	}
 
+
+
 }
+
+
